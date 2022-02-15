e9f31ba74c40f0ec6811afc706ec240c2f436e77
awaltlov <awaltlov@redhat.com>
2022-02-15 19:25:27 +0100

    Add solution for second exercise in second lesson

---
+++ lesson_2/program.py
@@ -0,0 +1,9 @@
+
+vek = input('Zadej vek:')
+jmeno = 'Andrea'
+pracovni_pozice = 'Software Engineer'
+
+
+print('Jmenuji se ' + jmeno + ' a je mi ' + str(vek) + ' a pracuji na pozici ' + pracovni_pozice)
+print('Jmenuji se', jmeno, 'a je mi', vek, 'a pracuji na pozici', pracovni_pozice)
+print(f'Jmenuji se {jmeno} a je mi {vek} a pracuji na pozici {pracovni_pozice}')
