46fe5f777bb17fd63a8399f8c1955fc77bf261e3
Andrea Waltlova <awaltlov@redhat.com>
2022-02-22 19:03:41 +0100

    Add stuf from live lesson

---
+++ lesson_3/program.py
@@ -0,0 +1,19 @@
+# vek = int(input('Zadej vek: '))
+
+# if vek >= 18:
+#     print('Smis vstoupit')
+# elif vek>= 15:
+#     print('Smis si koupit jenom limcu')
+# else:
+#     print('Nesmis vstoupit')
+
+# print('Vitejte v eshopu')
+
+
+prsi = True
+mam_destnik = True
+
+if (prsi and mam_destnik) or (not prsi):
+    print('Nemam mokrou bundu')
+else:
+    print('Mam mokrou bundu')
