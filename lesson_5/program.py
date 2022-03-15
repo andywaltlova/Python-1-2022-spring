32942091ac00e663c0d6a59d6d2c910b8e1ade57
Andrea Waltlova <awaltlov@redhat.com>
2022-03-15 20:01:58 +0100

    Update code from live lesson 5

--- lesson_5/program.py
+++ lesson_5/program.py
@@ -1,7 +1,29 @@
+from math import pi
+
+
+def greet_user(name):
+    print(f'Hello {name}!')
+
+
+def return_bigger(a, b):
+    return a if a > b else b
+
+
+def twice_pi():
+    return pi * 2
+
+
+def code_me_later(par1, par2):
+    # TODO
+    pass
+
+
+# Typove anotace take na kodim
+# uplně na konci kapitoly https://kodim.cz/czechitas/progr2-python/zaklady-programovani-2/funkce
 def get_mark(
         points: int,
-        bonus_points: int=0,
-        minus_points: int=0) -> int:
+        bonus_points: int = 0,
+        minus_points: int = 0) -> int:
     if points + bonus_points <= 60:
         mark = 5
     elif points + bonus_points <= 70:
@@ -15,5 +37,3 @@ def get_mark(
     return mark
 
 print(get_mark(30, 0, 20))
-
-round()
