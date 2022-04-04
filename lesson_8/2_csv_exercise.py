2843b84bc2edb9435460a3adf08b31182f2e6abe
Andrea Waltlova <awaltlov@redhat.com>
2022-04-04 23:27:53 +0200

    dd csv solutions

--- lesson_8/2_csv_exercise.py
+++ lesson_8/2_csv_exercise.py
@@ -1,4 +1,9 @@
-# 1. Čtení
+import csv
+import json
+
+
+### 1. Čtení
+
 # Stáhnětě si soubor [starships.csv](star_wars/starships.csv)
 
 # 1. Načtěte data pomocí modulu csv - nechám na vás jestli do slovníku nebo do seznamů.
@@ -9,8 +14,40 @@
 # 4. Najděte loď, která má celkovou nejvyší kapacitu, tedy vejde se do ní nejvíc posádky a pasažérů (crew a passengers).
 #    Pokud některá z hodnot není číslo, použijte k ošetření buď podmínky nebo vyjímky.
 
+with open('star_wars/starships.csv', newline='') as csvfile:
+    reader = csv.DictReader(csvfile)
+    # for row in reader:
+    #     print(row)
+    rows = list(reader)
 
-# 2. Zápis
+no_ships = 0
+for ship in rows:
+    # Vyuzila jsem toho ze python vyhodnocuje podminky zleva a jelikoz chci aby platily vsechny,
+    # tak pokud prvni neplati, python uz se dal nediva
+    if ship['passengers'].isdigit() and int(ship['passengers']) >= 2:
+        print(ship['name'], ship['manufacturer'])
+        no_ships += 1
+
+# Případně lze udělat nový list pomocí list comprehension a pomocí funkce len() dostat cislo
+print(no_ships)
+
+
+max_capacity = 0
+best_ship = {}
+for ship in rows:
+    try:
+        passengers, crew = int(ship['passengers']), int(ship['crew'])
+        if passengers + crew > max_capacity:
+            max_capacity = passengers + crew
+            best_ship = ship
+    except ValueError:
+        # Pokud hodnota neni cislo, chci pokracovat dale v prochazeni dalsich lodi
+        continue
+print(best_ship)
+
+
+### 2. Zápis
+
 # Stáhnětě si soubor [vehicles.csv](star_wars/vehicles.csv)
 
 # 1. Načtěte data pomocí modulu csv - nechám na vás jestli do slovníku nebo do seznamů.
@@ -20,8 +57,32 @@
 # 4. Zapište do druhého csv souboru pouze subset parametrů, tedy vyberte si pouze sloupce které vás zajímají a ty zapiště do souboru.
 #    - opět nezapomeňte na header
 
+with open('star_wars/vehicles.csv', newline='') as csvfile:
+    reader = csv.DictReader(csvfile)
+    # for row in reader:
+    #     print(row)
+    rows = list(reader)
 
-# 3. (Bonus) Čtení pomocí csv, zápis pomocí json
+
+with open('starships_filtered.csv', 'w', newline='') as csvfile:
+    writer = csv.writer(csvfile)
+    writer.writerow(rows[0].keys())
+    for row in rows:
+        if row['vehicle_class'] == 'starfighter' or row['vehicle_class'] == 'gunship':
+            writer.writerow(row.values())
+
+with open('starships_subset.csv', 'w', newline='') as csvfile:
+    filednames = ['name', 'model', 'manufacturer']
+    writer = csv.DictWriter(csvfile, fieldnames=filednames)
+    writer.writeheader()
+    for row in rows:
+        new_row = {}
+        for field in filednames:
+            new_row[field] = row[field]
+        writer.writerow(new_row)
+
+
+### 3. (Bonus) Čtení pomocí csv, zápis pomocí json
 
 # Stáhnětě si soubor [characters.csv](star_wars/characters.csv)
 
@@ -29,3 +90,21 @@
 # 2. Zapiště data pomocí některé z funkcí z modulu `json` do json souboru.
 #    - Zapište do zvlášť json souboru pouze všechny ženy
 #    - Zapište do zvlášť json souboru pouze všechny muže
+
+with open('star_wars/characters.csv', newline='', encoding='utf-8') as csvfile:
+    reader = csv.DictReader(csvfile)
+    # for row in reader:
+    #     print(row)
+    rows = list(reader)
+
+with open('all.json', 'w', encoding='utf-8') as jsonfile:
+    json.dump(rows, jsonfile, indent=4)
+
+females = [char for char in rows if char['gender'] == 'female']
+males = [char for char in rows if char['gender'] == 'male']
+
+with open('females.json', 'w', encoding='utf-8') as jsonfile:
+    json.dump(females, jsonfile, indent=4)
+
+with open('males.json', 'w', encoding='utf-8') as jsonfile:
+    json.dump(males, jsonfile, indent=4)
