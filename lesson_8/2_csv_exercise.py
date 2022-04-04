6707f60715822f51ba54af6d0a7dc3ee12fea305
Andrea Waltlova <awaltlov@redhat.com>
2022-04-05 00:05:44 +0200

    Add template for lesson 8 csv exercises

---
+++ lesson_8/2_csv_exercise.py
@@ -0,0 +1,31 @@
+# 1. Čtení
+# Stáhnětě si soubor [starships.csv](star_wars/starships.csv)
+
+# 1. Načtěte data pomocí modulu csv - nechám na vás jestli do slovníku nebo do seznamů.
+# 2. Vypiště si data na výstup
+# 3. Vypiště pouze jména a výrobce lodí, které mají kapacitu pro pasažéry alespoň 2.
+#    Pokud hodnota není číslo, použijte k ošetření buď podmínky nebo vyjímky.
+#    - Kolik takových lodí je?
+# 4. Najděte loď, která má celkovou nejvyší kapacitu, tedy vejde se do ní nejvíc posádky a pasažérů (crew a passengers).
+#    Pokud některá z hodnot není číslo, použijte k ošetření buď podmínky nebo vyjímky.
+
+
+# 2. Zápis
+# Stáhnětě si soubor [vehicles.csv](star_wars/vehicles.csv)
+
+# 1. Načtěte data pomocí modulu csv - nechám na vás jestli do slovníku nebo do seznamů.
+# 2. Vypiště si data na výstup
+# 3. Zapište do jednoho csv souboru pouze lodě, které mají `vehicle_class` starfighter nebo gunship.
+#    - nezapomeňtě na header, tedy první řádek s názvy sloupců. Mohli by se vám hodit metody values a keys u slovníků.
+# 4. Zapište do druhého csv souboru pouze subset parametrů, tedy vyberte si pouze sloupce které vás zajímají a ty zapiště do souboru.
+#    - opět nezapomeňte na header
+
+
+# 3. (Bonus) Čtení pomocí csv, zápis pomocí json
+
+# Stáhnětě si soubor [characters.csv](star_wars/characters.csv)
+
+# 1. Načtěte si data pomocí `DictReader` z modulu `csv`.
+# 2. Zapiště data pomocí některé z funkcí z modulu `json` do json souboru.
+#    - Zapište do zvlášť json souboru pouze všechny ženy
+#    - Zapište do zvlášť json souboru pouze všechny muže
