e80f871dd0f1eb17266be37a769bd4ae8a59625b
Andrea Waltlova <awaltlov@redhat.com>
2022-04-11 23:34:58 +0200

    Edit hw lesson 9

--- lesson_9/5_podminky_for_cykly.py
+++ lesson_9/5_podminky_for_cykly.py
@@ -1,8 +1,19 @@
+import sys
+
 # 7 Heslo
 # Napište program login.py, který na příkazovém řádku obdrží uživatelské jméno a heslo.
 # Program bude mít v sobě uloženo správné heslo a pokud jej uživatel zadá,
 # program vypíše něco ve smyslu “přístup povolen”. Zadá-li uživatel špatné heslo,
 # program odpoví “přístup odepřen”.
+
+spravne_heslo = '1234'
+
+username, heslo = sys.argv[1:3]
+
+if heslo == spravne_heslo:
+    print('přístup povolen')
+else:
+    print('přístup odepřen')
 
 # 8 Převod na USD
 # Napište program usd.py, který bude umět převádět měnu na americké dolary. Když program zavoláte takto
@@ -15,12 +26,17 @@
 
 # Jako přídavek můžete do svého programu přidat tolik měn, kolik se vám líbí.
 
+# TODO
+
 # 9 Banka
-# Napište program, který z textového souboru přečte seznam zůstatků na spořících účtech a vypíše tyto zůstatky navýšené o 2.5% úrok.
+# Napište program, který z textového souboru přečte seznam zůstatků na spořících účtech
+# a vypíše tyto zůstatky navýšené o 2.5% úrok.
 
 # Vypište každý navýšený zůstatek na samostatný řádek.
 # Vypište jen ty zůstatky, které nejsou záporné.
 # Zkuste jednotlivé řádky očíslovat. Každý řádek tedy bude obsahovat číslo řádku a výsledný zůstatek.
+
+# TODO
 
 # 10 Hadanky - jen na koukani
 
@@ -28,20 +44,36 @@
 # Napište program, který o zadaném seznamu celých čísel rozhodne,
 # zda jsou čísla v tomto seznamu vzestupně seřazena.
 
+# TODO
+
 # 12 Písemky
 # Napište program, který obdrží seznam známek z písemek a na výstup vypíše souhrn toho,
 # kolik bylo dohromady jedniček, kolik dvojek, kolik trojek a tak dále.
+pisemky_znamky = [1,1,1,4,5]
+
+for znamka in [1, 2, 3, 4, 5]:
+    print(f'{znamka} se objevila {pisemky_znamky.count(znamka)}')
 
 # 13 Maximum
 # Zkuste napsat program, který na vstupu obdrží seznam čísel a najde mezi nimi nejvyšší číslo.
 # Pozor, bez použití funkce max().
+nums = [ 34, 56, 78, 23, -1, 32]
+maximum = nums[0]
+for num in nums[1:]:
+    if num > maximum:
+        maximum = num
+print(maximum)
 
 # 14 Druhé maximum
 # Zkuste napsat program, který na vstupu obdrží seznam čísel a najde mezi nimi druhé nejvyšší číslo.
 # Opět bez použití funkce max().
 
+# TODO
+
 # 15 K-té maximum
-# Napište program, který dostane na příkazové řádce posloupnost čísel. První číslo udává, kolikáté největší číslo chceme ve zbytku zadaných čísel najít. Můžeme tak chtít třeba páté největší číslo z 6, 1, 3, 8, 4, 7, 2
+# Napište program, který dostane na příkazové řádce posloupnost čísel.
+# První číslo udává, kolikáté největší číslo chceme ve zbytku zadaných čísel najít.
+# Můžeme tak chtít třeba páté největší číslo z 6, 1, 3, 8, 4, 7, 2
 
 # $ python3 kmax.py 5 6 1 3 8 4 7 2
 # 3
@@ -49,9 +81,7 @@
 
 # Možností, jak tento úkol vyřešit, je více. Nebojte se zagooglit, nebojte se být kreativní.
 
-
-
-
+# TODO
 
 # 16 Ruleta
 # Na obrázku vidíte rozložení čísel na klasické Francouzské ruletě.
@@ -68,52 +98,52 @@ Klíčová slova and a or jsou popsaná zde:
 http://nove.kodim.cz/czechitas/progr2-python/zaklady-programovani-2/podminky-2
 """
 
-cislo = input("Zadej čislo: ")
-cislo = int(cislo)
+# cislo = input("Zadej čislo: ")
+# cislo = int(cislo)
 
-if cislo == 0:
-    print("Číslo je 0.")
-else:
-    if cislo % 2 == 0:
-        print("Číslo je sudé.")
-    else:
-        print("Číslo je liché.")
-    if cislo <= 10 or (cislo >= 19 and cislo <= 28):
-        if cislo % 2 == 0:
-            print("Číslo je černé.")
-        else:
-            print("Číslo je červené.")
-    else:
-        if cislo % 2 == 1:
-            print("Číslo je černé.")
-        else:
-            print("Číslo je červené.")
+# if cislo == 0:
+#     print("Číslo je 0.")
+# else:
+#     if cislo % 2 == 0:
+#         print("Číslo je sudé.")
+#     else:
+#         print("Číslo je liché.")
+#     if cislo <= 10 or (cislo >= 19 and cislo <= 28):
+#         if cislo % 2 == 0:
+#             print("Číslo je černé.")
+#         else:
+#             print("Číslo je červené.")
+#     else:
+#         if cislo % 2 == 1:
+#             print("Číslo je černé.")
+#         else:
+#             print("Číslo je červené.")
 
 # 17 Přestupný rok
 # Napište program, který po zadání kalendářního roku vypíše, zda jde o rok přestupný, či nikoliv.
 # Letopočet je přestupný, pokud je dělitelný čtyřmi.
 # Roky, které jsou dělitelné 100 jsou ovšem přestupné pouze tehdy, jsou-li zároveň dělitelné 400.
 
-rok = input("Zadej rok: ")
-rok = int(rok)
-if rok % 4 == 0:
-    if rok % 100 == 0:
-        if rok % 400 == 0:
-            print("Rok je přestupný.")
-        else:
-            print("Rok není přestupný")
-    else:
-        print("Rok je přestupný.")
-else:
-    print("Rok není přestupný.")
+# rok = input("Zadej rok: ")
+# rok = int(rok)
+# if rok % 4 == 0:
+#     if rok % 100 == 0:
+#         if rok % 400 == 0:
+#             print("Rok je přestupný.")
+#         else:
+#             print("Rok není přestupný")
+#     else:
+#         print("Rok je přestupný.")
+# else:
+#     print("Rok není přestupný.")
 
-"""
-Alternativní řešení pomocí klíčových slov and a or.
-Klíčová slova and a or jsou popsaná zde: 
-http://nove.kodim.cz/czechitas/progr2-python/zaklady-programovani-2/podminky-2
-"""
+# """
+# Alternativní řešení pomocí klíčových slov and a or.
+# Klíčová slova and a or jsou popsaná zde: 
+# http://nove.kodim.cz/czechitas/progr2-python/zaklady-programovani-2/podminky-2
+# """
 
-if rok % 4 == 0 and (rok % 100 != 0 or rok % 400 == 0):
-    print("Rok je přestupný.")
-else:
-    print("Rok není přestupný")
+# if rok % 4 == 0 and (rok % 100 != 0 or rok % 400 == 0):
+#     print("Rok je přestupný.")
+# else:
+#     print("Rok není přestupný")
