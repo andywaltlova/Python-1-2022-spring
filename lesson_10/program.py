c4890e41fc5b972e96640d0f7deb8f8b7d9b9cde
Andrea Waltlova <awaltlov@redhat.com>
2022-04-18 20:21:41 +0200

    Outline lesson 10

--- lesson_10/program.py
+++ lesson_10/program.py
@@ -1,1 +1,17 @@
 import pandas as pd
+
+# Documentation: https://pandas.pydata.org/docs/
+
+# read_csv, read_json, index
+# info(), describe(), columns
+# loc, iloc
+# head, tail
+# max, min
+# to_csv, to_json
+
+#####
+
+# selekce jako v sql, sloupce, radky
+# logicke operatory
+# isin()
+# prevod dataframu na list, reset_index
