e0149fb792dbf63398e9f76cf498b08548a9daf1
awaltlov <awaltlov@redhat.com>
2022-04-25 22:05:43 +0200

    Add outline python file lesson 11

---
+++ lesson_11/program.py
@@ -0,0 +1,13 @@
+# opakovani nacteni, loc, iloc, selekce, export do souboru
+# isin, prevod na seznam seznamu
+
+
+import wget
+
+# wget.download('https://kodim.cz/czechitas/python-data/datova-analyza/pandas-agregace/assets/u202.csv')
+# wget.download('https://kodim.cz/czechitas/python-data/datova-analyza/pandas-agregace/assets/u203.csv')
+# wget.download('https://kodim.cz/czechitas/python-data/datova-analyza/pandas-agregace/assets/u302.csv')
+
+# Práce s chybějícími hodnotami
+# Spojovani - concat, merge
+# Groupby
