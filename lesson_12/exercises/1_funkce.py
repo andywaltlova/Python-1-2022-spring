bc7e028548dc22709fa50e859f78276ff9774021
awaltlov <awaltlov@redhat.com>
2022-05-03 17:28:33 +0200

    Add template for lesson 12 exercises

---
+++ lesson_12/exercises/1_funkce.py
@@ -0,0 +1,14 @@
+# 1 Obsah elipsy
+# Tentokrát chceme spočítat plochu pozemku ve tvaru elipsy jako na obrázku.
+# Z matematiky víme, že známe-li šířku a výšku elipsy, její obsah je polovina šířky krát polovina výšky krát číslo pí. 
+# Napište funkci elipseArea, která spočítá plochu pozemku dle zadané šířky a výšky. Číslo pí najdete v modulu math jako math.pi.
+
+# 2 Větší ze dvou čísel
+# Napište funkci jménem max2, který vrátí větší ze dvou zadaných čísel.
+
+# 3 Geometrický průměr
+# Napište funkci jménem gmean, která spočítá takzvaný geometrický průměr ze zadaného seznamu čísel.
+# Geometrický průměr n čísel se spočítá tak, že se všechny hodnoty navzájem vynásobí a z výsledného součinu se spočítá n-tá odmocnina.
+
+# 4 Větší ze tří čísel
+# Napište funkci jménem max3, který vrátí největší ze tří zadaných čísel.
